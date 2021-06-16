70c36b651db728289c07c3e330e46f3cb9bf9e23
Kevin Dias <kevindias@MacBook-Air.lan>
2021-06-16 18:26:05 +0100

    base code

---
+++ lib/apis/fireBaseCalls.dart
@@ -0,0 +1,134 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:uni_mindelo/utils/constants/errorConstants.dart';
+import 'package:uni_mindelo/utils/services/dialogService.dart';
+import 'package:uni_mindelo/utils/services/loaderService.dart';
+import 'package:uni_mindelo/utils/services/storage.service.dart';
+import 'package:uni_mindelo/views/home/home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _auth = FirebaseAuth.instance;
+final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+login(email, password, BuildContext context) async {
+  try {
+    showLoader();
+
+    await _auth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) => {
+              saveData(user_uid, value.user!.uid),
+              dismissLoader(),
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Home()))
+              //print(value.user!.uid);
+            });
+  } on FirebaseAuthException catch (error) {
+    dismissLoader();
+    print(error.code);
+    switch (error.code) {
+      case userDontExists:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.USER_DONT_EXISTS',
+                  title: 'DIALOG.HEADER.ERROR',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      case wrongPassword:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.WRONG_PASSWORD',
+                  title: 'DIALOG.HEADER.ERROR',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      case tooManyRequests:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.TOO_MANY_REQUESTS',
+                  title: 'DIALOG.HEADER.WARNING',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      case invalidEmail:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.INVALID_EMAIL',
+                  title: 'DIALOG.HEADER.ERROR',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      default:
+    }
+    // print(error.code);
+    return error.code;
+  }
+}
+
+forgotPassword(email, BuildContext context) async {
+  try {
+    showLoader();
+    await _auth.sendPasswordResetEmail(email: email).then((_value) => {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return DialogService(
+                    subTitle: 'FORGOT_PASSWORD.EMAIL_SEND',
+                    title: 'DIALOG.HEADER.SUCCESS',
+                    dismissOnPopLogin: true);
+              }),
+          dismissLoader()
+        });
+  } on FirebaseAuthException catch (error) {
+    dismissLoader();
+    switch (error.code) {
+      case userDontExists:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.USER_DONT_EXISTS',
+                  title: 'DIALOG.HEADER.ERROR',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      case tooManyRequests:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.TOO_MANY_REQUESTS',
+                  title: 'DIALOG.HEADER.WARNING',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      case invalidEmail:
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return DialogService(
+                  subTitle: 'ERRORS.INVALID_EMAIL',
+                  title: 'DIALOG.HEADER.ERROR',
+                  dismissOnPopLogin: false);
+            });
+        break;
+      default:
+    }
+  }
+}
+
+getUserData(user_uid)  {
+  return _fireStore
+      .collection('Users')
+      .doc(user_uid)
+      .get();
+}
---
+++ lib/utils/services/dialogService.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:giffy_dialog/giffy_dialog.dart';
+import 'package:uni_mindelo/components/colors.dart';
+import 'package:uni_mindelo/views/auth/login/login.dart';
+
+class DialogService extends StatelessWidget {
+  final String subTitle;
+  final String title;
+  bool dismissOnPopLogin;
+  DialogService({required this.subTitle, required this.title, required this.dismissOnPopLogin});
+
+  dialogContent(BuildContext context) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return NetworkGiffyDialog(
+      image: Image.asset(
+        "assets/images/loginBaseDialog.gif",
+        fit: BoxFit.cover,
+      ),
+      entryAnimation: EntryAnimation.LEFT,
+      title: Text(
+        translate(title),
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
+      ),
+      description: Text(
+        translate(subTitle),
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 14.0),
+      ),
+      onlyCancelButton: true,
+      buttonCancelText: Text(translate('DIALOG.DIALOG_BT.OK')),
+      buttonCancelColor: PrimaryColor,
+      onCancelButtonPressed: () {
+        if(dismissOnPopLogin == true){
+          Navigator.of(context).push(MaterialPageRoute(builder : (context) => Login()));
+        }
+        else{
+          Navigator.pop(context, true);
+        }
+
+      },
+      // onOkButtonPressed: () {},
+    );
+  }
+}
